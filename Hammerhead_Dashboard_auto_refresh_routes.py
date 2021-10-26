35679179253f07ef055b94b35748f877722bae2f
Halbelunge <benjamin.boes@campus.tu-berlin.de>
2021-10-26 21:21:24 +0200

    Update Hammerhead_Dashboard_auto_refresh_routes.py
    Added the if __name__ == '__main__' construct to the script

--- Hammerhead_Dashboard_auto_refresh_routes.py
+++ Hammerhead_Dashboard_auto_refresh_routes.py
@@ -7,43 +7,87 @@ import json
 from bs4 import BeautifulSoup
 from OpenSSL import crypto, SSL
 
-#The PW Informations you have to fill out manually
-PW = ['enter your username here','enter your password here','enter your profile number here']
-#with this parameter you can set the inverval in that should be refreshed
-refresh_minutes = 15 #at the moment max 59 minutes!
-#to minimize the server communication to the necessary minimum, a night pause is added. The 2 parameters
-#specify in which time interval of the day should be refresed
-#as an example [6,22] means that only between 6:00 and 21:59 the routes are reloaded 
-refresh_houer_time_frame = [6,22]
+def main():
+    #The PW Informations you have to fill out manually
+    PW = ['enter your username here','enter your password here','enter your profile number here']
+    #with this parameter you can set the inverval in that should be refreshed
+    refresh_minutes = 15 #at the moment max 59 minutes!
+    #to minimize the server communication to the necessary minimum, a night pause is added. The 2 parameters
+    #specify in which time interval of the day should be refresed
+    #as an example [6,22] means that only between 6:00 and 21:59 the routes are reloaded 
+    refresh_houer_time_frame = [6,22]
 
-#this is the login url to that the login data is send
-url = 'https://dashboard.hammerhead.io/v1/auth/token'
+    #this is the login url to that the login data is send
+    url = 'https://dashboard.hammerhead.io/v1/auth/token'
 
-#this is the time the last time was logged in
-start_time = datetime.datetime.now()
-#set the last logged in time to one hour ago, to beeing sure a new login is done
-start_time = start_time - datetime.timedelta(hours = 1)
-#set the default time frame for the auth token to 3600sec. This will later be set to the real expire time frame
-expire_time_seconds = 3600
+    #this is the time the last time was logged in
+    start_time = datetime.datetime.now()
+    #set the last logged in time to one hour ago, to beeing sure a new login is done
+    start_time = start_time - datetime.timedelta(hours = 1)
+    #set the default time frame for the auth token to 3600sec. This will later be set to the real expire time frame
+    expire_time_seconds = 3600
 
-#copied default headers from browser to look not to suspicies
-headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/x-www-form-urlencoded',
-	'Host': 'dashboard.hammerhead.io',
-    'Origin': 'https://dashboard.hammerhead.io',
-    'Referer': 'https://dashboard.hammerhead.io/auth/signin',
-    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin'
-}
+    #copied default headers from browser to look not to suspicies
+    headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Host': 'dashboard.hammerhead.io',
+        'Origin': 'https://dashboard.hammerhead.io',
+        'Referer': 'https://dashboard.hammerhead.io/auth/signin',
+        'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin'
+    }
+
+    #this creates the payload for the login with the login data and specifies the login type via password
+    login_data = {
+        'grant_type': 'password',
+        'username': PW[0],
+        'password': PW[1]    
+    }
+
+    print('Creating Cert')
+    cert_gen()
+    print('Cert created') 
+    while True:
+        nowhour = datetime.datetime.now().hour
+        if nowhour > (refresh_houer_time_frame[0] - 1) and nowhour < refresh_houer_time_frame[1]:
+            with requests.Session() as s:
+                timedifference = datetime.datetime.now() - start_time
+                if timedifference.seconds < (expire_time_seconds - (refresh_minutes * 60)):
+                    print('enough rest time '+str(60 - (timedifference.seconds / 60))+' minutes')
+                else:
+                    print('logging in')
+                    r = s.post(url, headers=headers, data=login_data, verify=True,  cert =('./selfsigned.crt','./private.key'),auth=HTTPBasicAuth(PW[0], PW[1]))
+                    if r.status_code != 200:
+                        print('Error during login. Maybe Password or Username wrong')
+                        print('waiting for 30 minutes before retry')
+                        time.sleep(60 * 30)
+                        continue
+                    start_time = datetime.datetime.now()
+                    data_json = json.loads(r.content)
+                    print(data_json)
+                    headers['Authorization'] = 'Bearer ' + data_json['access_token']
+                    expire_time_seconds = int(data_json['expires_in'])
+                    print('logged in, refresh list')
+                r = s.post('https://dashboard.hammerhead.io/v1/users/'+PW[2]+'/routes/sync', headers=headers, verify=True,  cert =('./selfsigned.crt','./private.key'))
+                if r.status_code != 200:
+                    print('Error during refresh. Maybe User ID wrong')
+                    print('waiting for 30 minutes before retry')
+                    time.sleep(60 * 30)
+                    continue
+                print(r.content)
+        else:
+            print('outside of refresh time frame')
+        time.sleep(60*refresh_minutes)
+
 
 #function for creating an ssl cert for the https communication. This will be once created on start-up and is valid 10 years
 def cert_gen(
@@ -84,44 +128,5 @@ def cert_gen(
     with open(KEY_FILE, "wt") as f:
         f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
 
-#this creates the payload for the login with the login data and specifies the login type via password
-login_data = {
-    'grant_type': 'password',
-    'username': PW[0],
-    'password': PW[1]    
- }
-
-print('Creating Cert')
-cert_gen()
-print('Cert created') 
-while True:
-    nowhour = datetime.datetime.now().hour
-    if nowhour > (refresh_houer_time_frame[0] - 1) and nowhour < refresh_houer_time_frame[1]:
-        with requests.Session() as s:
-            timedifference = datetime.datetime.now() - start_time
-            if timedifference.seconds < (expire_time_seconds - (refresh_minutes * 60)):
-                print('enough rest time '+str(60 - (timedifference.seconds / 60))+' minutes')
-            else:
-                print('logging in')
-                r = s.post(url, headers=headers, data=login_data, verify=True,  cert =('./selfsigned.crt','./private.key'),auth=HTTPBasicAuth(PW[0], PW[1]))
-                if r.status_code != 200:
-                    print('Error during login. Maybe Password or Username wrong')
-                    print('waiting for 30 minutes before retry')
-                    time.sleep(60 * 30)
-                    continue
-                start_time = datetime.datetime.now()
-                data_json = json.loads(r.content)
-                print(data_json)
-                headers['Authorization'] = 'Bearer ' + data_json['access_token']
-                expire_time_seconds = int(data_json['expires_in'])
-                print('logged in, refresh list')
-            r = s.post('https://dashboard.hammerhead.io/v1/users/'+PW[2]+'/routes/sync', headers=headers, verify=True,  cert =('./selfsigned.crt','./private.key'))
-            if r.status_code != 200:
-                print('Error during refresh. Maybe User ID wrong')
-                print('waiting for 30 minutes before retry')
-                time.sleep(60 * 30)
-                continue
-            print(r.content)
-    else:
-        print('outside of refresh time frame')
-    time.sleep(60*refresh_minutes)
+if __name__ == '__main__':
+    main()
